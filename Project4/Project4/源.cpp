b4e43a00dfefb49d8418b56c6f3a10cb8704cfbf
LTP name <LTP400@qq.com>
2022-05-08 16:21:16 +0800

    helloworld 爱你

---
+++ Project4/Project4/源.cpp
@@ -0,0 +1,15 @@
+#include <Windows.h>
+int WINAPI WinMain(HINSTANCE hINSTANCE, HINSTANCE hPrevINSTANCE, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(NULL, L" 张晓艺 我爱你", L"大鹏", 0);
+
+	return 0;
+}
+/*
+#include<windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, PSTR szCmdLine, int iCmdShow)
+{
+	MessageBox(NULL, TEXT("第一个WIN32程序"), TEXT("打招呼"), MB_ABORTRETRYIGNORE);
+	return 0;
+} 作者：WRONGrestart https ://www.bilibili.com/read/cv9476048 出处：bilibili*/
